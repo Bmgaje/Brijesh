dc8639104045b5a09c7268027a5e53835c3a1dc1
Brijesh Gajera <gaje0006@algonquinlive.com>
2022-11-08 18:16:47 -0500

    "Commit 4 Added extra lines"

--- app/src/main/java/algonquin/cst2355/brijesh/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/brijesh/MainActivity.java
@@ -1,134 +1,83 @@
 package algonquin.cst2355.brijesh;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
-import static android.widget.Toast.LENGTH_SHORT;
-
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-/**
- * this is MainActivity class.
- * @author Brijesh4205
- * @since 5/11/2022
- * @version 1.0
- */
-
-
-
-
-
 public class MainActivity extends AppCompatActivity {
-    /* this hold the text at the center of screen*/
-    private TextView tv=null;
-    /* this holds and let the user to change the input*/
-    private EditText et=null;
-    /* this lets the user to click it and change the activity*/
-    private Button btn = null;
-    @Override
 
+    Button loginButton;
+    EditText emailEditText;
+
+
+    private static String TAG = "MainActivity";
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //loads xml file on screen:
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textView);
-        et = findViewById(R.id.password);
-        btn = findViewById(R.id.loginButton);
-        btn.setOnClickListener(clk -> {
-            String password = et.getText().toString();
-            if(checkPasswordComplexity(password)){
-                tv.setText("Password meets all requirements");
-            }
-            else{
-                tv.setText("you shall not pass");
-            }
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        Log.d( TAG, "Message");
 
-            checkPasswordComplexity(password);
+        loginButton = findViewById(R.id.loginButton);
+        emailEditText = findViewById(R.id.emailEditText);
 
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("LoginName", "");
+        emailEditText.setText(emailAddress);
+
+        loginButton.setOnClickListener( clk-> {
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            nextPage.putExtra( "EmailAddress", emailEditText.getText().toString() );
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LoginName", emailEditText.getText().toString());
+            editor.apply();
+            startActivity(nextPage);
         });
     }
 
-    /**
-     *
-     * @param password The string object that we are checking
-     * @return returns true if all conditions are present and false if not
-     */
-    public boolean checkPasswordComplexity(String password) {
 
-
-        boolean foundUppercase;
-        boolean foundLowercase;
-        boolean foundNumber;
-        boolean foundSpecial;
-        foundUppercase = foundLowercase = foundNumber = foundSpecial = false;
-
-
-        for (int i = 0; !foundLowercase && i < password.length(); i++) {
-            foundLowercase = Character.isLowerCase(password.charAt(i));
-        }
-
-        for (int i = 0; !foundUppercase && i < password.length(); i++) {
-            foundUppercase = Character.isUpperCase(password.charAt(i));
-        }
-
-        for (int i = 0; !foundNumber && i < password.length(); i++) {
-            foundNumber = Character.isDigit(password.charAt(i));
-        }
-        for (int i = 0; !foundSpecial && i < password.length(); i++) {
-            foundSpecial = isspecialcharacter(password.charAt(i));
-        }
-
-
-/**
- * toast messages for all the errors
- * @author Brijesh 4205
- * @since 5/11/2022
- * @version 1.0
- */
-
-        Toast nouppercase = Toast.makeText(getApplicationContext(), "missing an uppercase letter", LENGTH_SHORT);
-        Toast nolowercase = Toast.makeText(getApplicationContext(), "missing a lowercase letter", LENGTH_SHORT);
-        Toast nodigit = Toast.makeText(getApplicationContext(), "missing a digit", LENGTH_SHORT);
-        Toast nospecial = Toast.makeText(getApplicationContext(), "missing special characters", LENGTH_SHORT);
-
-        if (!foundLowercase) {
-            nolowercase.show();
-        }
-        if (!foundUppercase) {
-            nouppercase.show();
-        }
-        if (!foundNumber) {
-            nodigit.show();
-        }
-       if(!foundSpecial){
-            nospecial.show();
-        }
-        return foundLowercase && foundUppercase && foundNumber && foundSpecial;
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( TAG, "The application is now visible on the screen" );
     }
 
-    private boolean isspecialcharacter(char c) {
-        {
-            switch(c){
-                case'!':
-                case'@':
-                case'#':
-                case'$':
-                case'%':
-                case'^':
-                case'&':
-                case'*':
-                case'?':
-                    return true;
-                default:
-                    return false;
-            }
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( TAG, "The application is now responding to user input" );
     }
 
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.w( TAG, "The application no longer responds to user input" );
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.w( TAG, "The application is no longer visible" );
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.w( TAG, "Any memory used by the application is freed" );
+    }
 
 }
