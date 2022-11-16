131e1dcde90fbdfec4694f1412cd2a3dd5ba1013
Brijesh Gajera <gaje0006@algonquinlive.com>
2022-11-15 19:29:34 -0500

    "Week 6 "

--- app/src/main/java/algonquin/cst2355/brijesh/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/brijesh/MainActivity.java
@@ -1,131 +1,16 @@
 package algonquin.cst2355.brijesh;
 
-import static android.widget.Toast.LENGTH_SHORT;
-
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-/**
- * this is MainActivity class.
- * @author Brijesh4205
- * @since 5/11/2022
- * @version 1.0
- */
-
 
 
 
 
 public class MainActivity extends AppCompatActivity {
-    /* this hold the text at the center of screen*/
-    private TextView tv=null;
-    /* this holds and let the user to change the input*/
-    private EditText et=null;
-    /* this lets the user to click it and change the activity*/
-    private Button btn = null;
-    @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //loads xml file on screen:
-        setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textView);
-        et = findViewById(R.id.password);
-        btn = findViewById(R.id.loginButton);
-        btn.setOnClickListener(clk -> {
-            String password = et.getText().toString();
-            if(checkPasswordComplexity(password)){
-                tv.setText("Password meets all requirements");
-            }
-            else{
-                tv.setText("you shall not pass");
-            }
-
-            checkPasswordComplexity(password);
-
-        });
-    }
-
-    /**
-     *
-     * @param password The string object that we are checking
-     * @return returns true if all conditions are present and false if not
-     */
-    public boolean checkPasswordComplexity(String password) {
-
-
-        boolean foundUppercase;
-        boolean foundLowercase;
-        boolean foundNumber;
-        boolean foundSpecial;
-        foundUppercase = foundLowercase = foundNumber = foundSpecial = false;
-
-
-        for (int i = 0; !foundLowercase && i < password.length(); i++) {
-            foundLowercase = Character.isLowerCase(password.charAt(i));
-        }
-
-        for (int i = 0; !foundUppercase && i < password.length(); i++) {
-            foundUppercase = Character.isUpperCase(password.charAt(i));
-        }
-
-        for (int i = 0; !foundNumber && i < password.length(); i++) {
-            foundNumber = Character.isDigit(password.charAt(i));
-        }
-        for (int i = 0; !foundSpecial && i < password.length(); i++) {
-            foundSpecial = isspecialcharacter(password.charAt(i));
-        }
-
-
-/**
- * toast messages for all the errors that might happen to
- */
-
-        Toast nouppercase = Toast.makeText(getApplicationContext(), "missing an uppercase letter", LENGTH_SHORT);
-        Toast nolowercase = Toast.makeText(getApplicationContext(), "missing a lowercase letter", LENGTH_SHORT);
-        Toast nodigit = Toast.makeText(getApplicationContext(), "missing a digit", LENGTH_SHORT);
-        Toast nospecial = Toast.makeText(getApplicationContext(), "missing special characters", LENGTH_SHORT);
-
-        if (!foundLowercase) {
-            nolowercase.show();
-        }
-        if (!foundUppercase) {
-            nouppercase.show();
-        }
-        if (!foundNumber) {
-            nodigit.show();
-        }
-       if(!foundSpecial){
-            nospecial.show();
-        }
-        return foundLowercase && foundUppercase && foundNumber && foundSpecial;
 
     }
-
-    private boolean isspecialcharacter(char c) {
-        {
-            switch(c){
-                case'!':
-                case'@':
-                case'#':
-                case'$':
-                case'%':
-                case'^':
-                case'&':
-                case'*':
-                case'?':
-                    return true;
-                default:
-                    return false;
-            }
-        }
-    }
-
-
 }
