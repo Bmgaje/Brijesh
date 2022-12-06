cf3ee8f1f02e7a313de40771f1fe2dfe8595986e
Brijesh Gajera <gaje0006@algonquinlive.com>
2022-12-06 15:26:23 -0500

    "Week 10 "

--- app/src/main/java/algonquin/cst2355/brijesh/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/brijesh/MainActivity.java
@@ -13,13 +13,14 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
