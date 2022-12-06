e751847b41541f731fbb8d237a3af42400edec7b
Brijesh Gajera <gaje0006@algonquinlive.com>
2022-12-06 13:28:10 -0500

    "Week 6 "

--- app/src/main/java/algonquin/cst2355/brijesh/ChatMessage.java
+++ app/src/main/java/algonquin/cst2355/brijesh/ChatMessage.java
@@ -1,13 +1,54 @@
 package algonquin.cst2355.brijesh;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class ChatMessage {
 
-    String message;
-    String timeSent;
-    boolean isSentButton;
+    @PrimaryKey (autoGenerate = true)
+    @ColumnInfo(name = "id" )
+    protected int id;
+    @ColumnInfo(name = "message")
+    public String message;
+    @ColumnInfo(name = "TimeSent")
+    public String timeSent;
+    @ColumnInfo(name = "SendOrRecieve")
+    public boolean isSentButton;
 
-    void ChatRoom(String m, String t, boolean sent){
-        message = m;
+    public  ChatMessage(){};
+
+    ChatMessage(String m, String t, boolean sent)
+    {
+
+        message =m;
         timeSent = t;
         isSentButton = sent;
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public void setTimeSent(String timeSent) {
+        this.timeSent = timeSent;
+    }
+
+    public boolean isSentButton() {
+        return isSentButton;
+    }
+
+    public void setSentButton(boolean sentButton) {
+        isSentButton = sentButton;
+    }
 }
+
